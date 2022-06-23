c673d52c0aa37bff50fd574d3e4e6e071c19fd2a
Mounir El Omri <pyth0n.h1ss.01@gmail.com>
2022-06-23 23:32:36 +0200

    Setup for ping API

--- routing/routing.go
+++ routing/routing.go
@@ -2,9 +2,22 @@ package routing
 
 import (
 	"github.com/gorilla/mux"
+	"net/http"
+	"sentinel/logger"
 	"sentinel/middleware"
 )
 
 func SetupRouter(router *mux.Router) {
 	router.Use(middleware.LoggingMiddleware, middleware.RecoveryPanicMiddleware)
+
+	setupPingRouter(router)
+}
+
+func setupPingRouter(router *mux.Router) {
+	router.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("Server ON"))
+		if err != nil {
+			logger.LogMessageInRed("The server is not available.")
+		}
+	})
 }
