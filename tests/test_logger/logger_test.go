ec8ff6fb34c3636ee680983e30fa075a574d6018
Mounir El Omri <pyth0n.h1ss.01@gmail.com>
2022-06-28 00:10:50 +0200

    Added test for custom logger

---
+++ tests/test_logger/logger_test.go
@@ -0,0 +1,35 @@
+package test_logger
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestCustomLogger(t *testing.T) {
+	var (
+		message             = "logger message"
+		expectedMessageType = "string"
+	)
+
+	if reflect.TypeOf(message) != reflect.TypeOf(expectedMessageType) {
+		t.Errorf("Got %v (%T), wanted %T", message, message, expectedMessageType)
+	}
+}
+
+func TestCustomFatalLogger(t *testing.T) {
+	var (
+		message             = "fatal logger message"
+		err                 = fmt.Errorf("fatal logger error")
+		expectedMessageType = "string"
+		expectedErrType     = fmt.Errorf("error")
+	)
+
+	if reflect.TypeOf(message) != reflect.TypeOf(expectedMessageType) {
+		t.Errorf("Got %v (%T), wanted %T", message, message, expectedMessageType)
+	}
+
+	if reflect.TypeOf(err) != reflect.TypeOf(expectedErrType) {
+		t.Errorf("Got %v (%T), wanted %T", err, err, expectedErrType)
+	}
+}
