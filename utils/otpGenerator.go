641232c45450132dafc60f1bbd6d97b4eb451464
Mounir El Omri <pyth0n.h1ss.01@gmail.com>
2022-07-30 00:05:44 +0200

    Improved OTP generator security by using crypto/rand library

--- utils/otpGenerator.go
+++ utils/otpGenerator.go
@@ -1,21 +1,21 @@
 package utils
 
 import (
-	"math/rand"
+	"crypto/rand"
 	"sentinel/logger"
 )
 
-const otpChars = "1234567890"
-
 func GenerateOTP(length int) (string, error) {
+	const otpChars = "1234567890"
+
 	buffer := make([]byte, length)
 	_, err := rand.Read(buffer)
 	if err != nil {
 		logger.LogMessageInRed("Not able to generate OTP code, cannot read buffer: " + err.Error())
 		return "", err
 	}
-
 	otpCharsLength := len(otpChars)
+
 	for i := 0; i < length; i++ {
 		buffer[i] = otpChars[int(buffer[i])%otpCharsLength]
 	}
